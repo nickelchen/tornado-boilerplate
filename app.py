ee1a2409daabb5e682063c359efebbad10b69006
Nickel Chen <nickelchen0101@gmail.com>
2013-08-12 15:10:12 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+# coding: UTF-8
+import os
+import re
+import tornado.auth
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.autoreload
+import unicodedata
+from tornado.options import define, options
+from jinja2 import Template, Environment, FileSystemLoader
+from handlers import *
+import filter
+import session
+from mongoengine import *
+
+define("port", default=8888, help="run on the given port", type=int)
+define("mongo_host", default="127.0.0.1:3306", help="database host")
+define("mongo_database", default="quora", help="database name")
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+            (r"/login", LoginHandler),
+            (r"/register", RegisterHandler),
+            (r"/logout", LogoutHandler),
+        ]
+        settings = dict(
+            app_name=u"App website",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            cookie_secret="81o0TzKaPpGtYdkL5gEmGepeuuYi7EPnp2XdTP1o&Vo=",
+            login_url="/login",
+            session_secret='08091287&^(01',
+            session_dir=os.path.join(os.path.dirname(__file__), "tmp/session"),
+        )
+        self.session_manager = session.TornadoSessionManager(settings["session_secret"], settings["session_dir"])
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        # Connection MongoDB
+        connect(options.mongo_database)
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    instance = tornado.ioloop.IOLoop.instance()
+    tornado.autoreload.start(instance)
+    instance.start()
+
+if __name__ == "__main__":
+    main()
---
+++ handlers.py
@@ -0,0 +1,133 @@
+# coding: utf-8
+import sys
+import tornado.web
+import tornado.auth
+from jinja2 import Template, Environment, FileSystemLoader
+from bson.objectid import ObjectId
+
+import filter, utils, session
+from forms import *
+from models import *
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    def __init__(self, application, request, **kwargs):
+        tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
+        self.session = session.TornadoSession(application.session_manager, self)
+        self._title = self.settings['app_name']
+
+    def render_string(self,template,**args):
+        env = Environment(loader=FileSystemLoader(self.settings['template_path']))
+        env.filters['tags_name_tag'] = filter.tags_name_tag
+        env.filters['user_name_tag'] = filter.user_name_tag
+        env.filters['strftime'] = filter.strftime
+        env.filters['strfdate'] = filter.strfdate
+        env.filters['avatar'] = filter.avatar
+        env.filters['truncate_lines'] = utils.truncate_lines
+        template = env.get_template(template)
+        return template.render(settings=self.settings,
+                               title=self._title,
+                               notice_message=self.notice_message,
+                               current_user=self.current_user,
+                               static_url=self.static_url,
+                               modules=self.ui['modules'],
+                               xsrf_form_html=self.xsrf_form_html,
+                               **args)
+
+    def render(self, template, **args):
+        self.finish(self.render_string(template, **args))
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("user_id")
+        if not user_id: return None
+        try:
+          return User.objects(id = user_id).first()
+        except:
+          return None
+
+    def notice(self,msg,type = "success"):
+        type = type.lower()
+        if ['error','success','warring'].count(type) == 0:
+            type = "success"
+        self.session["notice_%s" % type] = msg 
+        self.session.save()
+
+    @property
+    def notice_message(self):
+        try:
+          msg = self.session['notice_error']
+          self.session['notice_error'] = None
+          self.session['notice_success'] = None
+          self.session['notice_warring'] = None
+          self.session.save()
+          if not msg:
+            return ""
+          else:
+            return msg
+        except:
+          return ""
+
+    def render_404(self):
+        raise tornado.web.HTTPError(404)
+
+    def set_title(self, str):
+        self._title = u"%s - %s" % (str,self.settings['app_name'])
+
+class HomeHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("home.html", asks=[])
+
+class LogoutHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.set_secure_cookie("user_id","")
+        self.redirect("/login")
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.set_title(u"登陆")
+        self.render("login.html")
+
+    def post(self):
+        self.set_title(u"登陆")
+        frm = LoginForm(self)
+        if not frm.validate():
+            frm.render("login.html")
+            return
+
+        password = utils.md5(frm.password)
+        user = User.objects(login=frm.login,
+                            password=password).first()
+        if not user:
+            frm.add_error("password", "不正确")
+            frm.render("login.html")
+
+        self.set_secure_cookie("user_id", str(user.id))
+        self.redirect(self.get_argument("next","/"))
+
+class RegisterHandler(BaseHandler):
+    def get(self):
+        self.set_title(u"注册")
+        user = User()
+        self.render("register.html", user=user)
+
+    def post(self):
+        self.set_title(u"注册")
+        frm = RegisterForm(self)
+        if not frm.validate():
+            frm.render("register.html")
+            return
+        
+        user = User(name=frm.name,
+                    login=frm.login,
+                    email=frm.email,
+                    password=utils.md5(frm.password))
+        try:
+          user.save()
+          self.set_secure_cookie("user_id",str(user.id))
+          self.redirect("/")
+        except Exception,exc:
+          self.notice(exc,"error")
+          frm.render("register.html")
+
